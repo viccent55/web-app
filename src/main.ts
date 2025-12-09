46e9272b71ec669d888f80296a96d9d7320140f2
viccent55 <viccent55@gmail.com>
2025-12-09 15:54:32 +0700

    add css

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "@/plugins/vuetify";
 import MasonryWall from "@yeger/vue-masonry-wall";
-import "@/assets/css/main.scss";
+import "./assets/css/main.scss";
 
 const app = createApp(App);
 app.use(pinia);
