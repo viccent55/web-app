2eb0bac5af748268d305a09c8be4e557e570563b
viccent55 <viccent55@gmail.com>
2025-12-24 10:04:17 +0700

    fix the latest bug! 241225

--- src/components/forbidden/NoteDialog.vue
+++ src/components/forbidden/NoteDialog.vue
@@ -13,7 +13,7 @@
   import useVariable from "@/composables/useVariable";
   import useSnackbar from "@/composables/useSnackbar";
   import { screenMode } from "@/hooks/useScreenMode";
-  import VideoPlayer from "@/components/Video.vue";
+  import VideoPlayer from "@/components/Videojs.vue";
 
   const noteDIalogRef = useTemplateRef("note-dialog");
   const { store, onCopy, route, formatDate, storeUser } = useVariable();
